b56b14fc8e3e2dbf6d3a37cdb5ab96c22506f69c
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-08-03 19:13:41 +0900

    Introduce redux container
    1. Redux の導入のみ。
    設定するだけなので、既存の動きは何も変わらない。
    
    2. configureStore では開発用・プロダクション用の設定を分けるため、少々特殊な書き方をしている。
    react-redux ではよく使われる定型文。
    
    3. ActionTypes を追加。

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { App } from './App';
+import { configureStore } from './store/configureStore';
 
-render(<App />, document.querySelector('#root'));
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
