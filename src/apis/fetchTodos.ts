113ce341f252c3048a63842a279adf2f7109433f
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-08-06 13:51:13 +0900

    Fetch todo data with jsonplaceholder and axios
    - Mock を通信を行う API 呼び出しに置き換える
        - https://jsonplaceholder.typicode.com
            - プロトタイピングなどで使える、仮のデータを提供してくれるサービス
            - GET /users/1, GET /users?_limit=3 など、標準的な REST に則ったリクエストが大体使える
            - 更新系の API (todo 追加等) は、後の修正で対応
        - 画像の場合は類似のサービスとして https://placeholder.com/ がある
            - 仮の画像を用意してくれるサービスとしては、
    - ajax によるデータ取得には axios というライブラリを使う

--- src/apis/fetchTodos.ts
+++ src/apis/fetchTodos.ts
@@ -1,18 +1,4 @@
-import { Todo } from '../types';
+import axios from 'axios';
 
-/**
- * Mock データを返す
- * Promise.resolve() に渡された値が then の response に入る
- * @example
- * fetchTodos().then(response => {
- *   console.log(response); // { data: [...] }
- *   console.log(response.data[0]); // { userId: 1, id: 1, title: 'foo', ... }
- * });
- */
-export const fetchTodos = () => {
-  const data: Todo[] = [
-    { userId: 1, id: 1, title: 'foo', completed: false },
-    { userId: 1, id: 2, title: 'bar', completed: false }
-  ];
-  return Promise.resolve({ data });
-};
+export const fetchTodos = () =>
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3');
